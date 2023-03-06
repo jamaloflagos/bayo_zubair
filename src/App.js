7eefdea36df21e11ac343045e97726e72884eedd
jamaloflagos <toyinjamal@gmail.com>
2023-03-06 20:54:52 +0100

    new commits

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import Wrap from './components/Wrap'
 function App() {
   return (
-    <div className="App">
-     Bayo
+    <div>
+      <Header />
+      <Wrap>
+        <Main />
+      </Wrap>
+      <Footer />
     </div>
   );
 }
